a513a67f210ff5156853cfce05b7b7f3b9addc81
Jay the chef <webseo76@gmail.com>
2021-09-06 14:29:56 -0700

    questions for quiz done

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+//select the start button
+var questions = [
+    {
+        question: 'What is the programming language of the web?',
+        answers: ['javaScript', 'python', 'java', 'mysql'],
+        rightAnswer: 'javaScript'
+    },
+    {
+        question: 'To assign a "string" to a variable you must surround the value by?',
+        answers: ['( )',  " ' '", ' " "', ' {} ', 'B or C'],
+        rightAnswer: 'B or C'
+    },
+    {
+        question: 'True or False, comments in your code are a waste of time.',
+        answers: ['true', 'false', 'sometimes'],
+        rightAnswer: 'false'
+    },
+    {
+        question: 'A variable declared inside of a function is only available within that function because of...?',
+        answers: ['dependence', 'variable dependence', 'scope', 'global space'],
+        rightAnswer: 'scope'
+    },
+    {
+        question: 'javaScript normally uses what type case, to declare variables with more than one word?',
+        answers: ['snake_case', 'kebab-case', 'camelCase', 'CapitalCamelCase'],
+        rightAnswer: 'camelCase'   
+    }   
+]
+//clicking the start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Countdown function
+// var timeleft = 10;
+// var downloadTimer = setInterval(function(){
+//   if(timeleft <= 0){
+//     clearInterval(downloadTimer);
+//     document.getElementById("countdown").innerHTML = "Finished";
+//   } else {
+//     document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+//   }
+//   timeleft -= 1;
+// }, 1000);
+// <div id="countdown"></div>
